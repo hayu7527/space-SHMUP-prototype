d16b42c54b39bb2ed0eebeeca76545e7ce2707d4
Hai <hayu7527@colorado.edu>
2019-09-29 22:39:58 -0600

    Hello

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")] 
+    public float speed = 10f;
+
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+            
+        }
+
+        set { this.transform.position = value; }
+    }
+    void Update()
+    {
+        Move();
+
+        if (bndCheck != null && !bndCheck.isOnScreen)
+        {
+            if (pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.tag == "ProjectileHero")
+        {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            print("Enemy hit by non-projectilehero:" +otherGO.name);
+        }
+    }
+}
